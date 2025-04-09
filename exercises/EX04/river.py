dd392e7bce29bc067ddeb4c58204173d20a178b5
riyasha3 <rgautam@unc.edu>
2025-04-09 19:33:50 -0400

    Progress on Exercise

--- exercises/EX04/river.py
+++ exercises/EX04/river.py
@@ -14,7 +14,7 @@ class River:
     fish: list[Fish]
 
     def __init__(self, num_fish: int, num_bears: int):
-        """New River with num_fish Fish and num_bears Bears"""
+        """New River with num_fish Fish and num_bears Bears."""
         self.day: int = 0
         self.fish: list[Fish] = []
         self.bears: list[Bear] = []
@@ -77,7 +77,7 @@ class River:
         return None
 
     def one_river_day(self):
-        """Simulate one day of life in the river"""
+        """Simulate one day of life in the river."""
         # Increase day by 1
         self.day += 1
         # Simulate one day for all Bears
